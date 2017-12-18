f0cde32e1349cca79737edf075f39e5c0b247a36
Hemanthkumar karumanchi <hk378@njit.edu>
2017-12-18 01:18:57 -0500

    21st commit

--- mvc/controllers/accountsController.php
+++ mvc/controllers/accountsController.php
@@ -121,7 +121,7 @@ class accountsController extends http\controller
 
         $record = accounts::findOne($_REQUEST['id']);
         $record->delete();
-        header("Location: index.php?page=accounts&action=all");
+        header("Location: index.php");
     }
 
     //this is to login, here is where you find the account and allow login or deny.
